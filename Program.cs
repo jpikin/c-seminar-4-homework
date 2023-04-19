07e62ea5f37d37e4cecde342a7158a2dde8a3f9d
j.pikin <pikin@bk.ru>
2023-04-19 13:32:11 +0500

    Task 29 added

--- Program.cs
+++ Program.cs
@@ -37,7 +37,16 @@
 // Console.WriteLine(GetSum(num));
 
 
-
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+// int [] arr = new int[8];
+// for (int i = 0; i < 8; i++)
+//     {
+//         Console.WriteLine($"Введите {i+1} число для добавления в массив");
+//         arr[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+// Console.Write("Ваш массив: [");
+// Console.Write(String.Join(", ", arr));
+// Console.WriteLine("]");
